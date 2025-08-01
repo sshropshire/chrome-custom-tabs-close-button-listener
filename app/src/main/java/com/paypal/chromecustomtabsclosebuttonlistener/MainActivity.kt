929702078a5598e4ed2b720cb73e76912eff72ef
sshropshire <sshropshire@paypal.com>
2025-08-01 10:29:14 -0500

    Update index.html to look for callback parameter.

--- app/src/main/java/com/paypal/chromecustomtabsclosebuttonlistener/MainActivity.kt
+++ app/src/main/java/com/paypal/chromecustomtabsclosebuttonlistener/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.paypal.chromecustomtabsclosebuttonlistener
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +17,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.net.toUri
 import com.paypal.chromecustomtabsclosebuttonlistener.ui.theme.ChromeCustomTabsCloseButtonListenerTheme
@@ -37,15 +40,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
+    // Ref: https://stackoverflow.com/a/67156998
+    val chromeCustomTabsLauncher =
+        rememberLauncherForActivityResult(StartActivityForResult()) { result ->
+            Log.d("CCT", result.toString())
+        }
+
     Column(
         modifier = modifier
     ) {
         Text(text = "Hello $name!")
         Button(onClick = {
-            val url = "https://sshropshire.github.io/chrome-custom-tabs-close-button-listener/"
-            val intent = CustomTabsIntent.Builder().build()
-            intent.launchUrl(context, url.toUri())
+            val uri = "https://sshropshire.github.io/chrome-custom-tabs-close-button-listener"
+                .toUri()
+                .buildUpon()
+                .appendQueryParameter("callback", "chrome-custom-tabs-demo://success")
+                .build()
+            val customTabsIntent = CustomTabsIntent.Builder().build()
+            val intent = customTabsIntent.intent
+            intent.data = uri
+            chromeCustomTabsLauncher.launch(intent)
         }) {
             Text(text = "Launch Chrome Custom Tab")
         }
