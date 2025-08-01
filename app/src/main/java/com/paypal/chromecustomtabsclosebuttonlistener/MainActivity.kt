732ae51be730baeac32b335d7ae2888580500083
sshropshire <sshropshire@paypal.com>
2025-08-01 10:01:21 -0500

    Add CCT and update index.html.

--- app/src/main/java/com/paypal/chromecustomtabsclosebuttonlistener/MainActivity.kt
+++ app/src/main/java/com/paypal/chromecustomtabsclosebuttonlistener/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.net.toUri
 import com.paypal.chromecustomtabsclosebuttonlistener.ui.theme.ChromeCustomTabsCloseButtonListenerTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +37,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    val context = LocalContext.current
+    Column(
         modifier = modifier
-    )
+    ) {
+        Text(text = "Hello $name!")
+        Button(onClick = {
+            val url = "https://sshropshire.github.io/chrome-custom-tabs-close-button-listener/"
+            val intent = CustomTabsIntent.Builder().build()
+            intent.launchUrl(context, url.toUri())
+        }) {
+            Text(text = "Launch Chrome Custom Tab")
+        }
+    }
 }
 
 @Preview(showBackground = true)
